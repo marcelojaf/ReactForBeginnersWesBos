7cc13d61de71d5f5daf729392e3ac14dab83df12
Marcelo Amador <marcelo@redbitdev.com>
2018-01-08 15:02:21 -0500

    copy of my course files

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyAPqY71f0hoiyaWAOWLTTkAKqpc-40-BUM",
+    authDomain: "catch-of-the-day-marcelo-31bcf.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-marcelo-31bcf.firebaseio.com",
+});
+
+export default base;
